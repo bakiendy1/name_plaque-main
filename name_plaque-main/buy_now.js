cc50eff2b8b1401426cadb2bda1cdc5b1cbe7e87
Bakiendy1 <bakiendy1@gmail.com>
2023-04-29 22:07:05 +0100

    First commit having buy_now.js file in it

---
+++ name_plaque-main/buy_now.js
@@ -0,0 +1,34 @@
+//Adding variables to store our data
+const buyBtn = document.querySelector('.buy-submit');
+const putName = document.querySelector('#guessField')
+const namePreview = document.querySelector('#prev');
+const successText = document.querySelector('.sucText');
+const warningText = document.querySelector('#warningArea');
+
+
+// Adding arrow function
+
+  const checkInput = () => {
+    if (putName.value.trim() === '') {
+        warningText.textContent = 'Please enter some letters.';
+        warningText.style.color ='red';
+        successText.textContent = '';
+     
+    } else if (namePreview.textContent === 'YOUR NAME') {
+        warningText.textContent = 'Please enter your custom name.';
+        successText.textContent = '';
+      
+    } else if (putName.value.length > 15){
+        warningText.textContent = 'You have exceeded the number of available letters';
+        successText.textContent = '';
+    } else { 
+        warningText.textContent = '';
+        successText.textContent = 'Successul!';
+        successText.style.color ='green';
+    }       
+  }
+
+
+//   Add event to carryout the action 
+
+buyBtn.addEventListener('click', checkInput);
